43126f154cbcc885cb76443bbfb1c73ec3dff110
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 14:51:44 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,8 @@ const HOSTNAME = "127.0.0.1";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data: Buffer) => {
+    console.log(data);
+
     connection.write("+PONG\r\n");
   });
 });
