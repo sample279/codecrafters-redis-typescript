c06bbc0ce792830e45c4aeee8fd4d8d2840ea8a2
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 15:19:34 +0530

    codecrafters submit [skip ci]

--- app/parseResp.ts
+++ app/parseResp.ts
@@ -1,8 +1,15 @@
 const parseResp = (data: Buffer) => {
   const parsedData = data.toString();
+  console.log(parsedData);
+
+  const input = [];
 
   if (parsedData.startsWith("*")) {
     const length = parsedData[1];
+    parsedData.split(`/\$\d+/`);
+    console.log(parsedData.split(`/\$\d+/`));
+);
+    
   }
 };
 
