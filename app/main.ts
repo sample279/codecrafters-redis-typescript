9d07f19874903e5a0a08cfbbf71703fa93f3738e
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 17:19:06 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,7 @@ const HOSTNAME = "127.0.0.1";
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data: Buffer) => {
     const tokens = parseResp(data);
-    const command: string = tokens[0].toLowerCase();
+    const command: string = tokens[0].toUpperCase();
 
     if (tokens.length === 0) return;
 
