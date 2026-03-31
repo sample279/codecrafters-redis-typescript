e4acbda25d7cae9cb6f3bfb65040d38505daa5f1
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 17:16:19 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,14 +8,13 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data: Buffer) => {
     const tokens = parseResp(data);
     const command: string = tokens[0].toLowerCase();
+    console.log(tokens[1]);
 
     if (command.startsWith("PING ")) {
       connection.write("+PONG\r\n");
       return;
     }
     if (command.startsWith("ECHO ")) {
-      console.log(tokens[1]);
-
       connection.write(`$${tokens[1].length}\r\n${tokens[1]}\r\n`);
       return;
     }
