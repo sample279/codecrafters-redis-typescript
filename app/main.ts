d7fbc0b68b6c1ac7c47660afe525b6d581e74e59
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 14:06:43 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,11 @@
-import * as net from "net";
+import { Server, createServer, Socket } from "net";
+import { socket } from "./socket";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment the code below to pass the first stage
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+const server: Server = createServer((socket: Socket) => {
+  socket.write("+PONG\r\n");
 });
-//
+
 server.listen(6379, "127.0.0.1");
