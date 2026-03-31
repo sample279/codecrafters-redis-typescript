6894f5f0383910e22c6a4b69368bc23987dc2b96
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-31 17:05:30 +0530

    codecrafters submit [skip ci]

--- app/parseResp.ts
+++ app/parseResp.ts
@@ -1,5 +1,11 @@
 const parseResp = (data: Buffer) => {
   const parsedData: string = data.toString();
+
+  const lines: string[] = parsedData
+    .split("\r\n")
+    .filter((line: string) => line.length > 0);
+
+  console.log(lines);
 };
 
 export { parseResp };
