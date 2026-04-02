ed75ccc26fd54ce66696d5f64a1a27fa9fa27e45
sample279 <224307167+sample279@users.noreply.github.com>
2026-04-03 01:42:38 +0530

    codecrafters submit [skip ci]

--- app/handlers.ts
+++ app/handlers.ts
@@ -37,11 +37,10 @@ const handlers: Record<string, (tokens: string[]) => string> = {
   },
 
   RPUSH: (tokens) => {
-    console.log(tokens);
     const key: string = tokens[0];
     const value: string = tokens[1];
 
-    storeUpdate(key, value);
+    return storeUpdate(key, value);
   },
 };
 
